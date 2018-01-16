86ca75d631b4f1c8cf660494fa910c2f428f3002
Jordan Jaffe <jordan.m.jaffe@gmail.com>
2018-01-16 07:27:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes'};
 function updatePlaylist(playlist, artist, song){
- playlist[] = song
+ playlist[Phil Ochs] = song
   return playlist
 }
