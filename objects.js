f3c5f4da52e5278b67334e21a6f941792374824e
Jordan Jaffe <jordan.m.jaffe@gmail.com>
2018-01-16 07:25:28 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,5 @@
+var playlist = {'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes'}
+function updatePlaylist(playlist, artist, song){
+ 
+  return Object.assign({}, playlist, { ['Phil Ochs']: "Here's to the state of missippi"})
+}
