4dd495afcc8ad56cfc5e8b9de7e9aa25a9dc7e77
Jordan Jaffe <jordan.m.jaffe@gmail.com>
2018-01-16 07:25:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
-var playlist = {'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes'}
+var playlist = {'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes'};
 function updatePlaylist(playlist, artist, song){
- 
-  return Object.assign({}, playlist, { ['Phil Ochs']: "Here's to the state of missippi"})
+ playlist['Phil Ochs']: "Here's to the state of missippi"
+  return playlist
 }
